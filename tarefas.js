2eb5a578fd51810912fd860e4a1a8e932a517da2
AllrightIsHere <gasilva912@gmail.com>
2021-12-15 11:14:03 -0300

    Exercício 0 e 1: Adicionando exemplos na página

--- tarefas.js
+++ tarefas.js
@@ -1,7 +1,7 @@
 const CATEGORIAS = Object.freeze({
-    LAZER = 'lazer',
-    COMPRAS = 'compras',
-    ESTUDOS = 'estudos'
+    LAZER: 'lazer',
+    COMPRAS: 'compras',
+    ESTUDOS: 'estudos'
 });
 
 class Tarefa {
@@ -14,9 +14,25 @@ class Tarefa {
         this.#categoria = categoria;
         this.#realizada = realizada;
     }
+
+    adicionaNaPagina(containerEl) {
+        const template = `
+        <li class="item-tarefa ${this.#realizada ? 'marcado' : ''} categoria-${this.#categoria}">${this.#nome}</li>
+        `;
+
+        containerEl.innerHTML += template;
+    }
+
+    static insereTarefasNaPagina(containerEl, tarefas) {
+        containerEl.innerHTML = '';
+        tarefas.forEach(tarefa => tarefa.adicionaNaPagina(containerEl));
+    }
 }
 
 const tarefas = [
     new Tarefa('Comprar leite', CATEGORIAS.COMPRAS, false),
     new Tarefa('Escutar chimbinha', CATEGORIAS.LAZER, true)
 ];
+
+const listaTarefasEl = document.querySelector('#lista-tarefas');
+Tarefa.insereTarefasNaPagina(listaTarefasEl, tarefas);
